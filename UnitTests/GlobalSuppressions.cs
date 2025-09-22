802a08baa0047fee875bb72f7fa306eec5dc8ebf
Todd West <twest820@gmail.com>
2025-09-22 10:15:50 -0700

    build: update dependencies

--- UnitTests/GlobalSuppressions.cs
+++ UnitTests/GlobalSuppressions.cs
@@ -9,6 +9,7 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "readability, debuggability", Scope = "member", Target = "~M:Mars.Seem.Test.PspTreeMeasurementSeries.#ctor(System.Int32,System.Int32,Mars.Seem.Tree.FiaCode)")]
 [assembly: SuppressMessage("Usage", "MSTEST0037:Use proper 'Assert' methods", Justification = "readability", Scope = "type", Target = "~T:Mars.Seem.Test.AssertV")]
 [assembly: SuppressMessage("Usage", "MSTEST0037:Use proper 'Assert' methods", Justification = "readability", Scope = "type", Target = "~T:Mars.Seem.Test.Mathematics")]
+[assembly: SuppressMessage("Usage", "MSTEST0037:Use proper 'Assert' methods", Justification = "readability", Scope = "type", Target = "~T:Mars.Seem.Test.OrganonInternal")]
 [assembly: SuppressMessage("Usage", "MSTEST0037:Use proper 'Assert' methods", Justification = "readability", Scope = "type", Target = "~T:Mars.Seem.Test.OrganonTest")]
 [assembly: SuppressMessage("Usage", "MSTEST0037:Use proper 'Assert' methods", Justification = "readability", Scope = "type", Target = "~T:Mars.Seem.Test.LowLevel")]
 [assembly: SuppressMessage("Usage", "MSTEST0037:Use proper 'Assert' methods", Justification = "readability", Scope = "type", Target = "~T:Mars.Seem.Test.PublicApi")]
